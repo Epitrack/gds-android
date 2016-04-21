7aee0408e02f28bf3324cf5734f2576e6a3e96f8
Igor Morais <mor41s.1gor@gmail.com>
2016-04-21 14:17:51 -0300

    Update notice layout

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/NoticeAdapter.java
+++ Android/Guardioes/app/src/main/java/com/epitrack/guardioes/view/NoticeAdapter.java
@@ -1,12 +1,9 @@
 package com.epitrack.guardioes.view;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epitrack.guardioes.R;
@@ -22,26 +19,28 @@ import butterknife.ButterKnife;
  * @author Igor Morais
  * @author Miqueias Lopes
  */
-public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.ViewHolder> {
+public class NoticeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_NOTICE = 1;
 
     private final NoticeListener listener;
-
-    private ViewHolder viewHolder;
 
     private List<Notice> noticeList = new ArrayList<>();
 
     public NoticeAdapter(final NoticeListener listener, final List<Notice> noticeList) {
-
-        if (listener == null) {
-            throw new IllegalArgumentException("The listener cannot be null.");
-        }
-
         this.listener = listener;
         this.noticeList = noticeList;
-
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(final View view) {
+            super(view);
+        }
+    }
+
+    public class NoticeViewHolder extends RecyclerView.ViewHolder {
 
         @Bind(R.id.text_view_title)
         TextView textViewTitle;
@@ -55,7 +54,7 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.ViewHolder
         @Bind(R.id.txt_view_date)
         TextView textViewDate;
 
-        public ViewHolder(final View view) {
+        public NoticeViewHolder(final View view) {
             super(view);
 
             ButterKnife.bind(this, view);
@@ -63,34 +62,56 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.ViewHolder
     }
 
     @Override
-    public NoticeAdapter.ViewHolder onCreateViewHolder(final ViewGroup viewGroup, final int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup viewGroup, final int viewType) {
+
+        if (viewType == TYPE_HEADER) {
+
+            final View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.notice_header, viewGroup, false);
+
+            return new ViewHolder(view);
+        }
         
         final View view = LayoutInflater.from(viewGroup.getContext())
-                                        .inflate(R.layout.notice_item, viewGroup, false);
+                .inflate(R.layout.notice_item, viewGroup, false);
 
-        return new ViewHolder(view);
+        return new NoticeViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final NoticeAdapter.ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
 
-        final Notice notice = noticeList.get(position);
+        if (holder instanceof NoticeViewHolder) {
 
-        viewHolder.textViewTitle.setText(notice.getTitle());
-        viewHolder.textViewClock.setText(notice.getClock());
-        viewHolder.textViewLike.setText(notice.getLike());
-        viewHolder.textViewDate.setText(notice.getPublicationDate());
+            final NoticeViewHolder viewHolder = (NoticeViewHolder) holder;
 
-        viewHolder.textViewTitle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onNoticeSelect(notice);
-            }
-        });
+            final Notice notice = noticeList.get(position);
+
+            viewHolder.textViewTitle.setText(notice.getTitle());
+            viewHolder.textViewClock.setText(notice.getClock());
+            viewHolder.textViewLike.setText(notice.getLike());
+            viewHolder.textViewDate.setText(notice.getPublicationDate());
+
+            viewHolder.textViewTitle.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(final View view) {
+
+                    listener.onNoticeSelect(notice);
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
         return noticeList.size();
+    }
+
+
+
+    @Override
+    public int getItemViewType(final int position) {
+        return position == 0 ? TYPE_HEADER : TYPE_NOTICE;
     }
 }
