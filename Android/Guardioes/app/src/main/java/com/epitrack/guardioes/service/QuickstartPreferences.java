bdd6617177aeeddc2102ea3ccfc88cc39bde1d6a
Igor Morais <mor41s.1gor@gmail.com>
2016-05-07 16:53:22 -0300

    Remove unused classes

--- Android/Guardioes/app/src/main/java/com/epitrack/guardioes/service/QuickstartPreferences.java
+++
@@ -1,9 +0,0 @@
-package com.epitrack.guardioes.service;
-
-/**
- * @author Miqueias Lopes
- */
-public class QuickstartPreferences {
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
-    public static final String REGISTRATION_COMPLETE = "registrationComplete";
-}
