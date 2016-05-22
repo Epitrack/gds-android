49639ac34ba4a0de601a09ba8dd7635ea520c6d7
Igor Morais <mor41s.1gor@gmail.com>
2016-05-22 14:09:16 -0300

    Implementing energy bar

---
+++ Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/EnergyFragment.java
@@ -0,0 +1,40 @@
+package com.epitrack.guardioes.view.game;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epitrack.guardioes.R;
+import com.epitrack.guardioes.view.base.BaseFragment;
+
+import butterknife.OnClick;
+
+/**
+ * @author Igor Morais
+ */
+public class EnergyFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup viewGroup, final Bundle bundle) {
+        return inflater.inflate(R.layout.energy, viewGroup, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle bundle) {
+
+        bind(view);
+    }
+
+    @OnClick(R.id.button_medal)
+    public void onMedal() {
+
+    }
+
+    @OnClick(R.id.button_trophy)
+    public void onTrophy() {
+
+    }
+}
