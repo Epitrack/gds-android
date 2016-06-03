2b45a018791b99348e12383a7c61adfaf25b9d79
Igor Morais <mor41s.1gor@gmail.com>
2016-06-02 23:44:49 -0300

    Implementing answer timer

--- Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/dialog/AnswerDialog.java
+++ Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/dialog/AnswerDialog.java
@@ -2,12 +2,15 @@ package com.epitrack.guardioes.view.game.dialog;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.epitrack.guardioes.R;
+import com.liulishuo.magicprogresswidget.MagicProgressCircle;
 
 /**
  * @author Igor Morais
@@ -15,6 +18,17 @@ import com.epitrack.guardioes.R;
 public class AnswerDialog extends DialogFragment implements View.OnClickListener {
 
     public static final String TAG = AnswerDialog.class.getSimpleName();
+
+    private static final int TIME = 15;
+    private static final int DELAY = 1000;
+    private static final float FACTOR = 15 / 1f;
+
+    private TextView textViewTime;
+    private MagicProgressCircle progressCircle;
+
+    private final Handler handler = new Handler();
+
+    private TimerHandler timer;
 
     @Override
     public void onCreate(final Bundle bundle) {
@@ -32,11 +46,59 @@ public class AnswerDialog extends DialogFragment implements View.OnClickListener
     @Override
     public void onViewCreated(final View view, @Nullable final Bundle bundle) {
 
+        textViewTime = (TextView) view.findViewById(R.id.text_view_time);
+
+        progressCircle = (MagicProgressCircle) view.findViewById(R.id.progress_timer);
+
         view.findViewById(R.id.image_button_close).setOnClickListener(this);
+
+        textViewTime.setText(String.valueOf(TIME));
+
+        load();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        handler.removeCallbacks(timer);
+    }
+
+    private void load() {
+
+        timer = new TimerHandler(TIME);
+
+        handler.postDelayed(timer, DELAY);
     }
 
     @Override
     public void onClick(final View view) {
         dismiss();
+    }
+
+    private class TimerHandler implements Runnable {
+
+        private static final int MIM = 0;
+
+        private int time;
+
+        public TimerHandler(final int time) {
+            this.time = time;
+        }
+
+        @Override
+        public void run() {
+
+            if (time > MIM) {
+
+                time--;
+
+                textViewTime.setText(String.valueOf(time));
+
+                progressCircle.setSmoothPercent((TIME - time) / FACTOR, DELAY);
+
+                handler.postDelayed(timer, DELAY);
+            }
+        }
     }
 }
