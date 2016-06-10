bda9c5f9ee7bce7abbf23b995fbea186af9e8961
Igor Morais <mor41s.1gor@gmail.com>
2016-06-10 16:11:28 -0300

    Adding flip animation on card

--- Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/GameActivity.java
+++ Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/GameActivity.java
@@ -8,10 +8,13 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epitrack.guardioes.R;
+import com.epitrack.guardioes.helper.Constants;
 import com.epitrack.guardioes.view.base.BaseAppCompatActivity;
+import com.epitrack.guardioes.view.game.model.Phase;
 
 import butterknife.Bind;
 
@@ -20,11 +23,17 @@ import butterknife.Bind;
  */
 public class GameActivity extends BaseAppCompatActivity implements AdapterView.OnItemClickListener {
 
+    private static final String ENERGY_FRAGMENT = "energy_fragment";
+
     @Bind(R.id.text_view_level)
     TextView textViewLevel;
 
     @Bind(R.id.grid_view)
     GridView gridView;
+
+    private Phase phase;
+
+    private EnergyFragment energyFragment;
 
     @Override
     protected void onCreate(@Nullable final Bundle bundle) {
@@ -32,14 +41,27 @@ public class GameActivity extends BaseAppCompatActivity implements AdapterView.O
 
         setContentView(R.layout.game);
 
-        gridView.setAdapter(new AnswerAdapter(this));
+        phase = (Phase) getIntent().getSerializableExtra(Constants.Bundle.PHASE);
+
+        textViewLevel.setText(getString(R.string.level, phase.getId()));
+
+        gridView.setAdapter(new PieceAdapter(this, phase.getPieceArray()));
         gridView.setOnItemClickListener(this);
+
+        getEnergyFragment().setEnergy(10);
     }
 
     @Override
     public void onItemClick(final AdapterView<?> adapterView, final View view, final int position, final long id) {
 
-        final AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flip_out);
+        flip(position, view);
+
+        //new AnswerDialog().show(getFragmentManager(), AnswerDialog.TAG);
+    }
+
+    private void flip(final int position, final View view) {
+
+        final AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flip);
 
         animator.setTarget(view);
 
@@ -53,10 +75,13 @@ public class GameActivity extends BaseAppCompatActivity implements AdapterView.O
             @Override
             public void onAnimationEnd(final Animator animation) {
 
-                final AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(GameActivity.this, R.animator.flip_out);
+                ((ImageView) view).setImageResource(phase.getPieceArray()[position]);
 
+                final AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(GameActivity.this, R.animator.alpha);
 
+                animator.setTarget(view);
 
+                animator.start();
             }
 
             @Override
@@ -71,7 +96,14 @@ public class GameActivity extends BaseAppCompatActivity implements AdapterView.O
         });
 
         animator.start();
+    }
 
-        //new AnswerDialog().show(getFragmentManager(), AnswerDialog.TAG);
+    private EnergyFragment getEnergyFragment() {
+
+        if (energyFragment == null) {
+            energyFragment = (EnergyFragment) getFragmentManager().findFragmentByTag(ENERGY_FRAGMENT);
+        }
+
+        return energyFragment;
     }
 }
