b7e0c756cd85bf1739149f5602701923416051bf
Igor Morais <mor41s.1gor@gmail.com>
2016-06-15 18:55:59 -0300

    Adding phase cards

--- Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/model/Piece.java
+++ Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/view/game/model/Piece.java
@@ -5,14 +5,424 @@ import com.epitrack.guardioes.R;
 public class Piece {
 
     public static final Integer[] ARCH = new Integer[] { R.drawable.image_arch_1,
-                                                         R.drawable.image_arch_2,
-                                                         R.drawable.image_arch_3,
-                                                         R.drawable.image_arch_4,
-                                                         R.drawable.image_arch_5,
-                                                         R.drawable.image_arch_6,
-                                                         R.drawable.image_arch_7,
-                                                         R.drawable.image_arch_8,
-                                                         R.drawable.image_arch_9 };
+            R.drawable.image_arch_2,
+            R.drawable.image_arch_3,
+            R.drawable.image_arch_4,
+            R.drawable.image_arch_5,
+            R.drawable.image_arch_6,
+            R.drawable.image_arch_7,
+            R.drawable.image_arch_8,
+            R.drawable.image_arch_9 };
+
+    public static final Integer[] ATHLETIC = new Integer[] { R.drawable.image_athletics_1,
+            R.drawable.image_athletics_2,
+            R.drawable.image_athletics_3,
+            R.drawable.image_athletics_4,
+            R.drawable.image_athletics_5,
+            R.drawable.image_athletics_6,
+            R.drawable.image_athletics_7,
+            R.drawable.image_athletics_8,
+            R.drawable.image_athletics_9 };
+
+    public static final Integer[] BADMINTON = new Integer[] { R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton,
+            R.drawable.image_badminton };
+
+    public static final Integer[] BASKETBALL = new Integer[] { R.drawable.image_basketball_1,
+            R.drawable.image_basketball_2,
+            R.drawable.image_basketball_3,
+            R.drawable.image_basketball_4,
+            R.drawable.image_basketball_5,
+            R.drawable.image_basketball_6,
+            R.drawable.image_basketball_7,
+            R.drawable.image_basketball_8,
+            R.drawable.image_basketball_9 };
+
+    public static final Integer[] BMX = new Integer[] { R.drawable.image_bmx_1,
+            R.drawable.image_bmx_2,
+            R.drawable.image_bmx_3,
+            R.drawable.image_bmx_4,
+            R.drawable.image_bmx_5,
+            R.drawable.image_bmx_6,
+            R.drawable.image_bmx_7,
+            R.drawable.image_bmx_8,
+            R.drawable.image_bmx_9 };
+
+    public static final Integer[] BOXING = new Integer[] { R.drawable.image_boxing_1,
+            R.drawable.image_boxing_2,
+            R.drawable.image_boxing_3,
+            R.drawable.image_boxing_4,
+            R.drawable.image_boxing_5,
+            R.drawable.image_boxing_6,
+            R.drawable.image_boxing_7,
+            R.drawable.image_boxing_8,
+            R.drawable.image_boxing_9 };
+
+    public static final Integer[] SLALOM = new Integer[] { R.drawable.image_slalom_1,
+            R.drawable.image_slalom_2,
+            R.drawable.image_slalom_3,
+            R.drawable.image_slalom_4,
+            R.drawable.image_slalom_5,
+            R.drawable.image_slalom_6,
+            R.drawable.image_slalom_7,
+            R.drawable.image_slalom_8,
+            R.drawable.image_slalom_9 };
+
+    public static final Integer[] BOATING = new Integer[] { R.drawable.image_boating_1,
+            R.drawable.image_boating_2,
+            R.drawable.image_boating_3,
+            R.drawable.image_boating_4,
+            R.drawable.image_boating_5,
+            R.drawable.image_boating_6,
+            R.drawable.image_boating_7,
+            R.drawable.image_boating_8,
+            R.drawable.image_boating_9 };
+
+    public static final Integer[] CYCLING_ROAD = new Integer[] { R.drawable.image_cycling_road_1,
+            R.drawable.image_cycling_road_2,
+            R.drawable.image_cycling_road_3,
+            R.drawable.image_cycling_road_4,
+            R.drawable.image_cycling_road_5,
+            R.drawable.image_cycling_road_6,
+            R.drawable.image_cycling_road_7,
+            R.drawable.image_cycling_road_8,
+            R.drawable.image_cycling_road_9 };
+
+    public static final Integer[] CYCLING_TRACK = new Integer[] { R.drawable.image_cycling_track_1,
+            R.drawable.image_cycling_track_2,
+            R.drawable.image_cycling_track_3,
+            R.drawable.image_cycling_track_4,
+            R.drawable.image_cycling_track_5,
+            R.drawable.image_cycling_track_6,
+            R.drawable.image_cycling_track_7,
+            R.drawable.image_cycling_track_8,
+            R.drawable.image_cycling_track_9 };
+
+    public static final Integer[] FENCING = new Integer[] { R.drawable.image_fencing_1,
+            R.drawable.image_fencing_2,
+            R.drawable.image_fencing_3,
+            R.drawable.image_fencing_4,
+            R.drawable.image_fencing_5,
+            R.drawable.image_fencing_6,
+            R.drawable.image_fencing_7,
+            R.drawable.image_fencing_8,
+            R.drawable.image_fencing_9 };
+
+    public static final Integer[] SOCCER = new Integer[] { R.drawable.image_soccer_1,
+            R.drawable.image_soccer_2,
+            R.drawable.image_soccer_3,
+            R.drawable.image_soccer_4,
+            R.drawable.image_soccer_5,
+            R.drawable.image_soccer_6,
+            R.drawable.image_soccer_7,
+            R.drawable.image_soccer_8,
+            R.drawable.image_soccer_9 };
+
+    public static final Integer[] ARTISTIC = new Integer[] { R.drawable.image_artistic_1,
+            R.drawable.image_artistic_2,
+            R.drawable.image_artistic_3,
+            R.drawable.image_artistic_4,
+            R.drawable.image_artistic_5,
+            R.drawable.image_artistic_6,
+            R.drawable.image_artistic_7,
+            R.drawable.image_artistic_8,
+            R.drawable.image_artistic_9 };
+
+    public static final Integer[] GOLF = new Integer[] { R.drawable.image_golf_1,
+            R.drawable.image_golf_2,
+            R.drawable.image_golf_3,
+            R.drawable.image_golf_4,
+            R.drawable.image_golf_5,
+            R.drawable.image_golf_6,
+            R.drawable.image_golf_7,
+            R.drawable.image_golf_8,
+            R.drawable.image_golf_9 };
+
+    public static final Integer[] GRECO_ROMAN = new Integer[] { R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman,
+            R.drawable.image_greco_roman };
+
+    public static final Integer[] HANDBALL = new Integer[] { R.drawable.image_handball_1,
+            R.drawable.image_handball_2,
+            R.drawable.image_handball_3,
+            R.drawable.image_handball_4,
+            R.drawable.image_handball_5,
+            R.drawable.image_handball_6,
+            R.drawable.image_handball_7,
+            R.drawable.image_handball_8,
+            R.drawable.image_handball_9 };
+
+    public static final Integer[] EQUESTRIAN_DRESSAGE = new Integer[] { R.drawable.image_equestrian_dressage_1,
+            R.drawable.image_equestrian_dressage_2,
+            R.drawable.image_equestrian_dressage_3,
+            R.drawable.image_equestrian_dressage_4,
+            R.drawable.image_equestrian_dressage_5,
+            R.drawable.image_equestrian_dressage_6,
+            R.drawable.image_equestrian_dressage_7,
+            R.drawable.image_equestrian_dressage_8,
+            R.drawable.image_equestrian_dressage_9 };
+
+    public static final Integer[] EQUESTRIANISM = new Integer[] { R.drawable.image_equestrianism_1,
+            R.drawable.image_equestrianism_2,
+            R.drawable.image_equestrianism_3,
+            R.drawable.image_equestrianism_4,
+            R.drawable.image_equestrianism_5,
+            R.drawable.image_equestrianism_6,
+            R.drawable.image_equestrianism_7,
+            R.drawable.image_equestrianism_8,
+            R.drawable.image_equestrianism_9 };
+
+    public static final Integer[] EQUESTRIAN_JUMPING = new Integer[] { R.drawable.image_equestrian_jumping_1,
+            R.drawable.image_equestrian_jumping_2,
+            R.drawable.image_equestrian_jumping_3,
+            R.drawable.image_equestrian_jumping_4,
+            R.drawable.image_equestrian_jumping_5,
+            R.drawable.image_equestrian_jumping_6,
+            R.drawable.image_equestrian_jumping_7,
+            R.drawable.image_equestrian_jumping_8,
+            R.drawable.image_equestrian_jumping_9 };
+
+    public static final Integer[] HOCKEY = new Integer[] { R.drawable.image_hockey_1,
+            R.drawable.image_hockey_2,
+            R.drawable.image_hockey_3,
+            R.drawable.image_hockey_4,
+            R.drawable.image_hockey_5,
+            R.drawable.image_hockey_6,
+            R.drawable.image_hockey_7,
+            R.drawable.image_hockey_8,
+            R.drawable.image_hockey_9 };
+
+    public static final Integer[] JUDO = new Integer[] { R.drawable.image_judo_1,
+            R.drawable.image_judo_2,
+            R.drawable.image_judo_3,
+            R.drawable.image_judo_4,
+            R.drawable.image_judo_5,
+            R.drawable.image_judo_6,
+            R.drawable.image_judo_7,
+            R.drawable.image_judo_8,
+            R.drawable.image_judo_9 };
+
+    public static final Integer[] LIFTING = new Integer[] { R.drawable.image_lifting_1,
+            R.drawable.image_lifting_2,
+            R.drawable.image_lifting_3,
+            R.drawable.image_lifting_4,
+            R.drawable.image_lifting_5,
+            R.drawable.image_lifting_6,
+            R.drawable.image_lifting_7,
+            R.drawable.image_lifting_8,
+            R.drawable.image_lifting_9 };
+
+    public static final Integer[] WRESTLING = new Integer[] { R.drawable.image_wrestling_1,
+            R.drawable.image_wrestling_2,
+            R.drawable.image_wrestling_3,
+            R.drawable.image_wrestling_4,
+            R.drawable.image_wrestling_5,
+            R.drawable.image_wrestling_6,
+            R.drawable.image_wrestling_7,
+            R.drawable.image_wrestling_8,
+            R.drawable.image_wrestling_9 };
+
+    public static final Integer[] AQUATIC_MARATHON = new Integer[] { R.drawable.image_aquatic_marathon_1,
+            R.drawable.image_aquatic_marathon_2,
+            R.drawable.image_aquatic_marathon_3,
+            R.drawable.image_aquatic_marathon_4,
+            R.drawable.image_aquatic_marathon_5,
+            R.drawable.image_aquatic_marathon_6,
+            R.drawable.image_aquatic_marathon_7,
+            R.drawable.image_aquatic_marathon_8,
+            R.drawable.image_aquatic_marathon_9 };
+
+    public static final Integer[] MOUNTAIN_BIKE = new Integer[] { R.drawable.image_mountain_bike_1,
+            R.drawable.image_mountain_bike_2,
+            R.drawable.image_mountain_bike_3,
+            R.drawable.image_mountain_bike_4,
+            R.drawable.image_mountain_bike_5,
+            R.drawable.image_mountain_bike_6,
+            R.drawable.image_mountain_bike_7,
+            R.drawable.image_mountain_bike_8,
+            R.drawable.image_mountain_bike_9 };
+
+    public static final Integer[] SYNCHRONIZED_SWIMMING = new Integer[] { R.drawable.image_synchronized_swimming_1,
+            R.drawable.image_synchronized_swimming_2,
+            R.drawable.image_synchronized_swimming_3,
+            R.drawable.image_synchronized_swimming_4,
+            R.drawable.image_synchronized_swimming_5,
+            R.drawable.image_synchronized_swimming_6,
+            R.drawable.image_synchronized_swimming_7,
+            R.drawable.image_synchronized_swimming_8,
+            R.drawable.image_synchronized_swimming_9 };
+
+    public static final Integer[] SWIMMING = new Integer[] { R.drawable.image_swimming_1,
+            R.drawable.image_swimming_2,
+            R.drawable.image_swimming_3,
+            R.drawable.image_swimming_4,
+            R.drawable.image_swimming_5,
+            R.drawable.image_swimming_6,
+            R.drawable.image_swimming_7,
+            R.drawable.image_swimming_8,
+            R.drawable.image_swimming_9 };
+
+    public static final Integer[] PENTATHLON = new Integer[] { R.drawable.image_pentathlon_1,
+            R.drawable.image_pentathlon_2,
+            R.drawable.image_pentathlon_3,
+            R.drawable.image_pentathlon_4,
+            R.drawable.image_pentathlon_5,
+            R.drawable.image_pentathlon_6,
+            R.drawable.image_pentathlon_7,
+            R.drawable.image_pentathlon_8,
+            R.drawable.image_pentathlon_9 };
+
+    public static final Integer[] WATER_POLO = new Integer[] { R.drawable.image_water_polo_1,
+            R.drawable.image_water_polo_2,
+            R.drawable.image_water_polo_3,
+            R.drawable.image_water_polo_4,
+            R.drawable.image_water_polo_5,
+            R.drawable.image_water_polo_6,
+            R.drawable.image_water_polo_7,
+            R.drawable.image_water_polo_8,
+            R.drawable.image_water_polo_9 };
+
+    public static final Integer[] ROWING = new Integer[] { R.drawable.image_rowing_1,
+            R.drawable.image_rowing_2,
+            R.drawable.image_rowing_3,
+            R.drawable.image_rowing_4,
+            R.drawable.image_rowing_5,
+            R.drawable.image_rowing_6,
+            R.drawable.image_rowing_7,
+            R.drawable.image_rowing_8,
+            R.drawable.image_rowing_9 };
+
+    public static final Integer[] RHYTHMIC = new Integer[] { R.drawable.image_rhythmic_1,
+            R.drawable.image_rhythmic_2,
+            R.drawable.image_rhythmic_3,
+            R.drawable.image_rhythmic_4,
+            R.drawable.image_rhythmic_5,
+            R.drawable.image_rhythmic_6,
+            R.drawable.image_rhythmic_7,
+            R.drawable.image_rhythmic_8,
+            R.drawable.image_rhythmic_9 };
+
+    public static final Integer[] RUGBY = new Integer[] { R.drawable.image_rugby_1,
+            R.drawable.image_rugby_2,
+            R.drawable.image_rugby_3,
+            R.drawable.image_rugby_4,
+            R.drawable.image_rugby_5,
+            R.drawable.image_rugby_6,
+            R.drawable.image_rugby_7,
+            R.drawable.image_rugby_8,
+            R.drawable.image_rugby_9 };
+
+    public static final Integer[] ORNAMENTAL = new Integer[] { R.drawable.image_ornamental_1,
+            R.drawable.image_ornamental_2,
+            R.drawable.image_ornamental_3,
+            R.drawable.image_ornamental_4,
+            R.drawable.image_ornamental_5,
+            R.drawable.image_ornamental_6,
+            R.drawable.image_ornamental_7,
+            R.drawable.image_ornamental_8,
+            R.drawable.image_ornamental_9 };
+
+    public static final Integer[] TAEKWONDO = new Integer[] { R.drawable.image_taekwondo_1,
+            R.drawable.image_taekwondo_2,
+            R.drawable.image_taekwondo_3,
+            R.drawable.image_taekwondo_4,
+            R.drawable.image_taekwondo_5,
+            R.drawable.image_taekwondo_6,
+            R.drawable.image_taekwondo_7,
+            R.drawable.image_taekwondo_8,
+            R.drawable.image_taekwondo_9 };
+
+    public static final Integer[] TENNIS = new Integer[] { R.drawable.image_tennis_1,
+            R.drawable.image_tennis_2,
+            R.drawable.image_tennis_3,
+            R.drawable.image_tennis_4,
+            R.drawable.image_tennis_5,
+            R.drawable.image_tennis_6,
+            R.drawable.image_tennis_7,
+            R.drawable.image_tennis_8,
+            R.drawable.image_tennis_9 };
+
+    public static final Integer[] TABLE_TENNIS = new Integer[] { R.drawable.image_table_tennis_1,
+            R.drawable.image_table_tennis_2,
+            R.drawable.image_table_tennis_3,
+            R.drawable.image_table_tennis_4,
+            R.drawable.image_table_tennis_5,
+            R.drawable.image_table_tennis_6,
+            R.drawable.image_table_tennis_7,
+            R.drawable.image_table_tennis_8,
+            R.drawable.image_table_tennis_9 };
+
+    public static final Integer[] SHOOTING = new Integer[] { R.drawable.image_shooting_1,
+            R.drawable.image_shooting_2,
+            R.drawable.image_shooting_3,
+            R.drawable.image_shooting_4,
+            R.drawable.image_shooting_5,
+            R.drawable.image_shooting_6,
+            R.drawable.image_shooting_7,
+            R.drawable.image_shooting_8,
+            R.drawable.image_shooting_9 };
+
+    public static final Integer[] TRAMPOLINE = new Integer[] { R.drawable.image_trampoline_1,
+            R.drawable.image_trampoline_2,
+            R.drawable.image_trampoline_3,
+            R.drawable.image_trampoline_4,
+            R.drawable.image_trampoline_5,
+            R.drawable.image_trampoline_6,
+            R.drawable.image_trampoline_7,
+            R.drawable.image_trampoline_8,
+            R.drawable.image_trampoline_9 };
+
+    public static final Integer[] TRIATHLON = new Integer[] { R.drawable.image_triathlon_1,
+            R.drawable.image_triathlon_2,
+            R.drawable.image_triathlon_3,
+            R.drawable.image_triathlon_4,
+            R.drawable.image_triathlon_5,
+            R.drawable.image_triathlon_6,
+            R.drawable.image_triathlon_7,
+            R.drawable.image_triathlon_8,
+            R.drawable.image_triathlon_9 };
+
+    public static final Integer[] SAIL = new Integer[] { R.drawable.image_sail_1,
+            R.drawable.image_sail_2,
+            R.drawable.image_sail_3,
+            R.drawable.image_sail_4,
+            R.drawable.image_sail_5,
+            R.drawable.image_sail_6,
+            R.drawable.image_sail_7,
+            R.drawable.image_sail_8,
+            R.drawable.image_sail_9 };
+
+    public static final Integer[] BEACH_VOLLEYBALL = new Integer[] { R.drawable.image_beach_volleyball_1,
+            R.drawable.image_beach_volleyball_2,
+            R.drawable.image_beach_volleyball_3,
+            R.drawable.image_beach_volleyball_4,
+            R.drawable.image_beach_volleyball_5,
+            R.drawable.image_beach_volleyball_6,
+            R.drawable.image_beach_volleyball_7,
+            R.drawable.image_beach_volleyball_8,
+            R.drawable.image_beach_volleyball_9 };
+
+    public static final Integer[] VOLLEYBALL = new Integer[] { R.drawable.image_volleyball_1,
+            R.drawable.image_beach_volleyball_2,
+            R.drawable.image_beach_volleyball_3,
+            R.drawable.image_beach_volleyball_4,
+            R.drawable.image_beach_volleyball_5,
+            R.drawable.image_beach_volleyball_6,
+            R.drawable.image_beach_volleyball_7,
+            R.drawable.image_beach_volleyball_8,
+            R.drawable.image_beach_volleyball_9 };
 
     private Piece() {
 
