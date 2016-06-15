2aa50d7cb9b2f227d2c1dc1d0ebefe28ef5f8745
Igor Morais <mor41s.1gor@gmail.com>
2016-06-15 18:55:33 -0300

    Adding update requester

--- Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/request/GameRequester.java
+++ Android/Guardioes/app/src/olympics/java/com/epitrack/guardioes/request/GameRequester.java
@@ -3,8 +3,15 @@ package com.epitrack.guardioes.request;
 import android.content.Context;
 
 import com.epitrack.guardioes.helper.Constants;
+import com.epitrack.guardioes.helper.DateFormat;
+import com.epitrack.guardioes.helper.Logger;
+import com.epitrack.guardioes.helper.Utility;
+import com.epitrack.guardioes.manager.PrefManager;
+import com.epitrack.guardioes.model.SingleUser;
+import com.epitrack.guardioes.model.User;
 import com.epitrack.guardioes.request.base.BaseRequester;
 import com.epitrack.guardioes.request.base.Method;
+import com.epitrack.guardioes.request.base.RequestException;
 import com.epitrack.guardioes.request.base.RequestListener;
 import com.epitrack.guardioes.request.base.Requester;
 import com.epitrack.guardioes.request.base.RequesterConfig;
@@ -16,14 +23,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Igor Morais
  */
 public class GameRequester extends BaseRequester {
+
+    private static final String TAG = GameRequester.class.getSimpleName();
 
     public GameRequester(final Context context) {
         super(context);
@@ -76,7 +90,7 @@ public class GameRequester extends BaseRequester {
 
     public void getQuestion(final RequestListener<List<Question>> listener) {
 
-        final String url = RequesterConfig.URL + "/game/questions/?lang=pt_BR";
+        final String url = "http://rest.guardioesdasaude.org/game/questions/?lang=pt_BR";
 
         listener.onStart();
 
@@ -93,7 +107,7 @@ public class GameRequester extends BaseRequester {
 
                         try {
 
-                            for (final JsonNode jsonNode : new ObjectMapper().readTree(response.getResult()).get(Constants.Json.QUESTION_LIST)) {
+                            for (final JsonNode jsonNode : new ObjectMapper().readTree(response.getResult())) {
 
                                 final Question question = new Question();
 
@@ -125,5 +139,154 @@ public class GameRequester extends BaseRequester {
                 }
             }
         });
+    }
+
+    public void update(final int energy, final int level, final Map<Integer, Boolean> pieceMap, final User user, final RequestListener<User> listener) {
+
+        final String url = "http://rest.guardioesdasaude.org/user/lookup/";
+
+        final Map<String, Object> bodyMap = new HashMap<>();
+
+        bodyMap.put("nick", user.getNick());
+        bodyMap.put("email", user.getEmail());
+        bodyMap.put("password", user.getPassword());
+        bodyMap.put("client", user.getClient());
+        bodyMap.put("dob", DateFormat.getDate(user.getDob()));
+        bodyMap.put("gender", user.getGender());
+        bodyMap.put("app_token", user.getAppToken());
+        bodyMap.put("race", user.getRace());
+        bodyMap.put("platform", user.getPlatform());
+        bodyMap.put("picture", user.getImage());
+        bodyMap.put("gcm_token", user.getGcmToken());
+        //bodyMap.put("xp", user.getEnergy());
+        //bodyMap.put("level", user.getLevel());
+        //bodyMap.put("puzzleMatriz", toIntArray(user.getPieceMap()));
+
+        listener.onStart();
+
+        new Requester(getContext()).request(Method.GET, url, getAuthHeaderMap(), bodyMap, new FutureCallback<Response<String>>() {
+
+            @Override
+            public void onCompleted(final Exception error, final Response<String> response) {
+
+                if (error == null) {
+
+                    if (isSuccess(response)) {
+
+                        try {
+
+                            final JSONObject json = new JSONObject(response.getResult());
+
+                            if (json.getBoolean("error")) {
+
+                                listener.onError(new RequestException());
+
+                            } else {
+
+                                loadUser(json.getJSONObject("data"));
+
+                                final User user = SingleUser.getInstance();
+
+                                if (new PrefManager(getContext()).put(Constants.Pref.USER, user)) {
+
+                                    listener.onSuccess(user);
+                                }
+                            }
+
+                        } catch (final JSONException e) {
+                            Logger.logDebug(TAG, e.getMessage());
+
+                            listener.onError(new RequestException());
+                        }
+
+                    } else {
+
+                        listener.onError(new RequestException());
+                    }
+
+                } else {
+
+                    listener.onError(error);
+                }
+            }
+        });
+    }
+
+    private int[] toIntArray(final Map<Integer, Boolean> pieceMap) {
+
+        final int[] pieceArray = new int[9];
+
+        for (int i = 0; i < pieceArray.length; i++) {
+            pieceArray[i] = pieceMap.get(i) ? 1 : 0;
+        }
+
+        return pieceArray;
+    }
+
+    private Map<Integer, Boolean> toMap(final int[] pieceArray) {
+
+        final Map<Integer, Boolean> pieceMap = new HashMap<>();
+
+        for (int i = 0; i < pieceArray.length; i++) {
+            pieceMap.put(i, pieceArray[i] == 1);
+        }
+
+        return pieceMap;
+    }
+
+    // TODO: Refactor soon..
+    private void loadUser(JSONObject json) {
+
+        try {
+
+            final SingleUser user = SingleUser.getInstance();
+
+            user.setNick(json.getString("nick"));
+            user.setEmail(json.getString("email"));
+            user.setGender(json.getString("gender"));
+            user.setId(json.getString("id"));
+            user.setRace(json.getString("race"));
+            user.setDob(json.getString("dob"));
+            user.setUserToken(json.getString("token"));
+
+            try {
+                user.setPath(json.getString("file"));
+
+            } catch (Exception e) {
+            }
+
+            try {
+                user.setImage(json.getInt("picture"));
+
+            } catch (Exception e) {
+
+            }
+
+            try {
+
+                user.setHashtags(Utility.toList(json.getJSONArray("hashtags")));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if (json.has("xp")) {
+
+                user.setEnergy(json.getInt("xp"));
+            }
+
+            if (json.has("level")) {
+                user.setEnergy(json.getInt("level"));
+            }
+
+            if (json.has("puzzleMatriz")) {
+
+               // user.setPieceMap(toMap(json.ge));
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
